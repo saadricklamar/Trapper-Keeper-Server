8d39c590cce6b2c63d0bab046118db286dae3e1a
raechel <odom.raechel@gmail.com>
2019-05-22 14:49:02 -0600

    start server set up

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.locals.notes = [
+  {title: 'test', task: [{name: 'task', id: 1, complete: false}], id: 2}
+];
+
+app.get('/api/v1/notes', (req, res) => {
+  res.status(200).json(app.locals.notes)
+});
+
+app.post('/api/v1/notes', (req, res) => {
+  const { title, task, id } = req.body;
+  if (!title || !task) return res.status(422).json('Please provide a title and task');
+  const newNote = {
+    id: Date.now(),
+    ...req.body
+  };
+  app.locals.notes.push(newNote);
+  res.status(201).json(newNote);
+})
+
+export default app;
---
+++ app.spec.js
@@ -0,0 +1,14 @@
+import request from 'supertest';
+import '@babel/polyfill';
+import app from './app';
+
+desribe('API', () => {
+
+  describe('GET /api/v1/notes', () => {
+    
+    it('should return a status of 200', () => {
+      const response = request(app).get('/api/v1/notes');
+      expect(response.statusCode).toBe(200);
+    })
+  })
+})
